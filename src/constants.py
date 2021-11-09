5688c90f6a14107cda2b33f7d4df3c60f3471652
Marino San Lorenzo <marinosanlorenzo@Marinos-MacBook-Pro.local>
2021-11-09 18:09:04 +0100

    1st commit

---
+++ src/constants.py
@@ -0,0 +1,8 @@
+
+class Constants:
+    URL_LINK_TO_DATA = 'URL_LINK_TO_DATA'
+
+
+params = {
+    Constants.URL_LINK_TO_DATA: "https://gitfront.io/r/katrienantonio/a29071bdc7b2f20f24268be573ef54b68c65168c/IABE-DS-module-1/raw/assignment/assignment_data.csv"
+}
