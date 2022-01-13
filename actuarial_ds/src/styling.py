e2b7f2fc451185235914ce9d56fdd124bed39dd3
Marino San Lorenzo <marinosanlorenzo@Marinos-MacBook-Pro.local>
2022-01-13 13:53:21 +0100

    .gitignore

---
+++ actuarial_ds/src/styling.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from src.constants import params, Constants
+from typing import Any
+
+__all__ = ['highlight_col']
+
+def highlight_col(x:pd.DataFrame, color='yellow') -> str:
+    return f'background-color: {color}'
