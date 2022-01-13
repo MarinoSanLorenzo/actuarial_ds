937086a02f657371f808f78a640146bc35cdaec7
Marino San Lorenzo <marinosanlorenzo@Marinos-MacBook-Pro.local>
2022-01-13 16:35:24 +0100

    bivariate analysis categorical variables2

--- src/plots.py
+++ src/plots.py
@@ -3,9 +3,32 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from src.constants import Constants, params
 from typing import List, Dict, Any
+from src.utils import get_avg_target_per_numerical_bin
 
-__all__ = ['plot_univariate_numerical_variables_distribution', 'plot_univariate_categorical_variables_distribution']
+__all__ = ['plot_univariate_numerical_variables_distribution', 'plot_univariate_categorical_variables_distribution', 'plot_avg_target_per_numerical_bin', 'plt_avg_target_per_category']
 
+def plt_avg_target_per_category(df:pd.DataFrame, categorical_variables:List[str], params:Dict[str, Any]) -> None:
+    nb_claims = params.get(Constants.NB_CLAIMS)
+    for feature in categorical_variables:
+        pd.DataFrame(df.groupby(feature)[nb_claims].mean()).sort_values(by=[nb_claims], ascending=False).plot.bar()
+        plt.title(f'Average number of claims per category of {feature.upper()}')
+        plt.ylabel(f'Average nb of claims')
+        plt.xlabel(f'{feature.upper()}')
+        plt.show()
+        plt.close()
+
+def plot_avg_target_per_numerical_bin(df:pd.DataFrame, numerical_variables:List[str], params:Dict[str, Any]) -> None:
+    nb_claims, claim_amount, var_to_exclude = params.get(Constants.NB_CLAIMS), params.get(Constants.CLAIM_AMOUNT), params.get(Constants.VARIABLES_TO_EXCLUDE)
+    for num_var in numerical_variables:
+        if not num_var in {*params.get(Constants.VARIABLES_TO_EXCLUDE), nb_claims, claim_amount}:
+            print(num_var)
+            avg_claim_frequency_per_bin = get_avg_target_per_numerical_bin(df, num_var, nb_claims)
+            avg_claim_frequency_per_bin.plot()
+            plt.ylabel(f'Average {Constants.NB_CLAIMS}')
+            plt.xticks(rotation=90)
+            plt.title(f'Average {Constants.NB_CLAIMS} per bin {num_var}')
+            plt.show()
+            plt.close()
 
 def plot_univariate_categorical_variables_distribution(df:pd.DataFrame, categorical_variables:List[str], params:Dict[str, Any]) -> None:
     for cat_var in categorical_variables:
--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,16 @@
-from typing import Dict, Tuple
+from typing import Dict, Tuple, List, Union
 import pandas as pd
 from collections import defaultdict
 
-__all__ = ['get_distribution', 'get_distribution_info_for_categorical_variables']
+__all__ = ['get_distribution', 'get_distribution_info_for_categorical_variables', 'get_avg_target_per_numerical_bin']
+
+
+def get_avg_target_per_numerical_bin(df:pd.DataFrame,feature:str, target:Union[str, List[str]]) -> Union[pd.DataFrame, pd.Series]:
+    df_cp = df.copy()
+    binned_feature = f'{feature}_bin_quantile_based'
+    df_cp[binned_feature] = pd.qcut(df[feature], q=10, precision=0, duplicates='drop')
+    return df_cp.groupby(binned_feature)[target].mean()
+
 
 def get_distribution_info_for_categorical_variables(df:pd.DataFrame) -> pd.DataFrame:
     '''
