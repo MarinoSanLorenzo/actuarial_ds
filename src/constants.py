8dc1aa696145c3d5dc8754cecad93c3f72bf782f
Marino San Lorenzo <marinosanlorenzo@Marinos-MacBook-Pro.local>
2022-01-15 03:22:54 +0100

    glm severity + frequency

--- src/constants.py
+++ src/constants.py
@@ -11,6 +11,7 @@ class Constants:
     TEST_SIZE = 'TEST_SIZE'
     RANDOM_STATE = 'RANDOM_STATE'
     LINK_TO_POSTAL_CODE_MAPPING = 'LINK_TO_POSTAL_CODE_MAPPING'
+    RENAMING_DUMMY_CODING_MAPPING = 'RENAMING_DUMMY_CODING_MAPPING'
 
 
 params = {
@@ -24,6 +25,41 @@ params = {
     Constants.MAX_CLAIM_AMOUNT: 500_000,
     Constants.TEST_SIZE:0.2,
     Constants.RANDOM_STATE:42,
-    Constants.LINK_TO_POSTAL_CODE_MAPPING: './postal_code_mapping.csv'
-
+    Constants.LINK_TO_POSTAL_CODE_MAPPING: './postal_code_mapping.csv', # source https://www.odwb.be/explore/dataset/code-postaux-belge/export/
+    Constants.RENAMING_DUMMY_CODING_MAPPING: {"duree":"duree",
+                                                "nbrtotc":"nbrtotc",
+                                                "chargtot":"chargtot",
+                                                "agecar_0-1":"agecar_zero_one",
+                                                "agecar_2-5":"agecar_two_five",
+                                                "agecar_6-10":"agecar_six_ten",
+                                                "agecar_>10":"agecar_higher_ten",
+                                                "sexp_Female":"sexp_Female",
+                                                "sexp_Male":"sexp_Male",
+                                                "fuelc_Gasoil":"fuelc_Gasoil",
+                                                "fuelc_Petrol":"fuelc_Petrol",
+                                                "split_Monthly":"split_Monthly",
+                                                "split_Once":"split_Once",
+                                                "split_Thrice":"split_Thrice",
+                                                "split_Twice":"split_Twice",
+                                                "usec_Private":"usec_Private",
+                                                "usec_Professional":"usec_Professional",
+                                                "fleetc_No":"fleetc_No",
+                                                "fleetc_Yes":"fleetc_Yes",
+                                                "sportc_No":"sportc_No",
+                                                "sportc_Yes":"sportc_Yes",
+                                                "coverp_MTPL":"coverp_MTPL",
+                                                "coverp_MTPL+":"coverp_MTPLplus",
+                                                "coverp_MTPL+++":"coverp_MTPLplusplusplus",
+                                                "powerc_66-110":"powerc_sixtysix_onehundredten",
+                                                "powerc_<66":"powerc_higher_sixtysix",
+                                                "powerc_>110":"powerc_higher_onehundredten",
+                                                "district_risk_group_high":"district_risk_group_high",
+                                                "district_risk_group_low":"district_risk_group_low",
+                                                "district_risk_group_medium":"district_risk_group_medium",
+                                                "AGEPH_bin_(16.999, 33.0]":"AGEPH_bin_seventeen_thirtythree",
+                                                "AGEPH_bin_(33.0, 41.0]":"AGEPH_bin_thirtythree_forty_one",
+                                                "AGEPH_bin_(41.0, 50.0]":"AGEPH_bin_forty_one_fifty",
+                                                "AGEPH_bin_(50.0, 61.0]":"AGEPH_bin_fifty_sixty_one",
+                                                "AGEPH_bin_(61.0, 95.0]":"AGEPH_bin_sixty_one_ninety_five"
+                                              }
 }
--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,47 @@
 from typing import Dict, Tuple, List, Union
 import pandas as pd
 from collections import defaultdict
+import numpy as np
 
-__all__ = ['get_distribution', 'get_distribution_info_for_categorical_variables', 'get_avg_target_per_numerical_bin']
+__all__ = ['get_distribution', 'get_distribution_info_for_categorical_variables', 'get_avg_target_per_numerical_bin', 'apply_mean_hot_categorical_encoding', 'bin_numerical_variables', 'get_reference_classes']
+
+
+def get_reference_classes(df:pd.DataFrame, target:str, exposure_name:str) -> List[str]:
+    reference_class_lst = []
+    df['claim_frequency'] = df[target] / df[exposure_name]
+    for cat_var in df.select_dtypes(include=['category']):
+        avg_frequency_per_category = pd.DataFrame(df.groupby(cat_var)['claim_frequency'].mean()).sort_values(
+            by=['claim_frequency'], ascending=False)
+        reference_class = avg_frequency_per_category.tail(1).last_valid_index()
+        print(f'The reference class of {cat_var} is {reference_class}')
+        reference_class_lst.append(f'{cat_var}_{reference_class}')
+    df.drop(columns='claim_frequency', inplace=True)
+    return reference_class_lst
+
+def bin_numerical_variables(df:pd.DataFrame, var_name:str, nb_bin:int=5) -> pd.DataFrame:
+    labels = [str(interval) for interval in list(pd.qcut(df[var_name], q=nb_bin).unique().categories)]
+    df[f'{var_name}_bin'] = pd.Categorical(pd.qcut(df[var_name], q=nb_bin, labels=labels))
+    return df
+
+def apply_mean_hot_categorical_encoding(df:pd.DataFrame, target:str, var_name:str, nb_bin:int=3, risk_groups:list=None) -> Tuple[pd.DataFrame, Dict]:
+    avg_frequency = pd.DataFrame(df.groupby(var_name)[target].mean())
+    avg_freq_df = pd.DataFrame(pd.qcut(avg_frequency[target], q=nb_bin))
+    categories = avg_freq_df[target].unique().categories
+
+    if not risk_groups:
+        risk_groups = [f'low', \
+                           f'medium', \
+                           f'high']
+    if nb_bin!= len(risk_groups):
+        raise ValueError(f'nb_bin {nb_bin} and lenght risk_groups {len(risk_groups)} should be the same!')
+
+    cond_list = [(avg_frequency[target] > category.left) & (avg_frequency[target] <= (category.right + 0.001)) for category in categories]
+
+    avg_frequency[f'{var_name}_risk_group'] = np.select(cond_list, risk_groups)
+    risk_group_mapping = avg_frequency[f'{var_name}_risk_group'].to_dict()
+    df[f'{var_name}_risk_group'] = pd.Categorical(df.district.replace(risk_group_mapping))
+    return df, risk_group_mapping
+
 
 
 def get_avg_target_per_numerical_bin(df:pd.DataFrame,feature:str, target:Union[str, List[str]]) -> Union[pd.DataFrame, pd.Series]:
@@ -17,7 +56,7 @@ def get_distribution_info_for_categorical_variables(df:pd.DataFrame) -> pd.DataF
     Construct a dataframe with information about the distribution of the categorical variables
     '''
     info = defaultdict(list)
-    cat_data = df.select_dtypes(include=['object'])
+    cat_data = df.select_dtypes(include=['object', 'category'])
     cols = cat_data.columns
     for col in cols:
         nb_unique_values, dist = get_distribution(df[col])
